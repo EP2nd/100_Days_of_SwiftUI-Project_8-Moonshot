bb3ad64865df059fb731ed91b63af5c275fe83bc
Edwin Przeźwiecki Jr <edwin.przezwiecki.jr@icloud.com>
2023-01-14 12:07:18 +0100

    Completion
    Solutions to the challenges are on their way.

---
+++ Moonshot/Views/MissionView.swift
@@ -0,0 +1,112 @@
+//
+//  MissionView.swift
+//  Moonshot
+//
+//  Created by Edwin Przeźwiecki Jr. on 13/01/2023.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    
+    struct CrewMember {
+        let role: String
+        let astronaut: Astronaut
+    }
+    
+    let mission: Mission
+    let crew: [CrewMember]
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ScrollView {
+                VStack {
+                    Image(mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: geometry.size.width * 0.6)
+                        .padding(.top)
+                    
+                    VStack(alignment: .leading) {
+                        Rectangle()
+                            .frame(height: 2)
+                            .foregroundColor(.lightBackground)
+                            .padding(.vertical)
+                        Text("Mission Highlights")
+                            .font(.title.bold())
+                            .padding(.bottom, 5)
+                        
+                        Text(mission.description)
+                        Rectangle()
+                            .frame(height: 2)
+                            .foregroundColor(.lightBackground)
+                            .padding(.vertical)
+                        
+                        Text("Crew")
+                            .font(.title.bold())
+                            .padding(.bottom, 5)
+                    }
+                    .padding(.horizontal)
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack {
+                            ForEach(crew, id: \.role) { crewMember in
+                                NavigationLink {
+                                    AstronautView(astronaut: crewMember.astronaut)
+                                } label: {
+                                    HStack(spacing: -10) {
+                                        Image(crewMember.astronaut.id)
+                                            .resizable()
+                                            .frame(width: 149.76, height: 103.68)
+                                            .clipShape(Circle())
+                                            .overlay(
+                                                Circle()
+                                                    .strokeBorder(crewMember.role == "Commander" || crewMember.role == "Command Pilot" ? .yellow : .gray, lineWidth: 2)
+                                            )
+                                        
+                                        VStack(alignment: .leading) {
+                                            Text(crewMember.astronaut.name)
+                                                .foregroundColor(.white)
+                                                .font(.headline)
+                                            Text(crewMember.role)
+                                                .foregroundColor(.secondary)
+                                        }
+                                        .padding(.leading)
+                                    }
+                                    .padding(.trailing)
+                                }
+                            }
+                        }
+                    }
+                }
+                .padding(.bottom)
+            }
+        }
+        .navigationTitle(mission.displayName)
+        .navigationBarTitleDisplayMode(.inline)
+        .background(.darkBackground)
+    }
+    
+    init(mission: Mission, astronauts: [String: Astronaut]) {
+        self.mission = mission
+        
+        self.crew = mission.crew.map { member in
+            if let astronaut = astronauts[member.name] {
+                return CrewMember(role: member.role, astronaut: astronaut)
+            } else {
+                fatalError("\(member.name)")
+            }
+        }
+    }
+}
+
+struct MissionView_Previews: PreviewProvider {
+    
+    static let missions: [Mission] = Bundle.main.decode("missions.json")
+    static let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    
+    static var previews: some View {
+        MissionView(mission: missions[0], astronauts: astronauts)
+            .preferredColorScheme(.dark)
+    }
+}
